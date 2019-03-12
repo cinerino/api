30a18cada37c6a490d8694a643b11f3b09cbbf2c
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2019-03-12 04:17:51 +0000

    ### Added - イベント在庫状況更新タスクを追加

--- src/jobs/run.ts
+++ src/jobs/run.ts
@@ -2,6 +2,17 @@
  * 非同期ジョブ
  */
 import abortTasks from './continuous/abortTasks/run';
+import makeTransactionExpired from './continuous/makeTransactionExpired/run';
+import reexportTransactionTasks from './continuous/reexportTransactionTasks/run';
+import retryTasks from './continuous/retryTasks/run';
+
+import onCanceledPlaceOrder from './continuous/onCanceledPlaceOrder/run';
+import onCanceledReturnOrder from './continuous/onCanceledReturnOrder/run';
+import onConfirmedPlaceOrder from './continuous/onConfirmedPlaceOrder/run';
+import onConfirmedReturnOrder from './continuous/onConfirmedReturnOrder/run';
+import onExpiredPlaceOrder from './continuous/onExpiredPlaceOrder/run';
+import onExpiredReturnOrder from './continuous/onExpiredReturnOrder/run';
+
 import cancelAccount from './continuous/cancelAccount/run';
 import cancelCreditCard from './continuous/cancelCreditCard/run';
 import cancelPointAward from './continuous/cancelPointAward/run';
@@ -9,33 +20,37 @@ import cancelSeatReservation from './continuous/cancelSeatReservation/run';
 import confirmReservation from './continuous/confirmReservation/run';
 import givePointAward from './continuous/givePointAward/run';
 import importScreeningEvents from './continuous/importScreeningEvents/run';
-import makeTransactionExpired from './continuous/makeTransactionExpired/run';
-import onCanceledPlaceOrder from './continuous/onCanceledPlaceOrder/run';
-import onCanceledReturnOrder from './continuous/onCanceledReturnOrder/run';
-import onConfirmedPlaceOrder from './continuous/onConfirmedPlaceOrder/run';
-import onConfirmedReturnOrder from './continuous/onConfirmedReturnOrder/run';
-import onExpiredPlaceOrder from './continuous/onExpiredPlaceOrder/run';
-import onExpiredReturnOrder from './continuous/onExpiredReturnOrder/run';
 import payAccount from './continuous/payAccount/run';
 import payCreditCard from './continuous/payCreditCard/run';
 import payMovieTicket from './continuous/payMovieTicket/run';
 import placeOrder from './continuous/placeOrder/run';
-import reexportTransactionTasks from './continuous/reexportTransactionTasks/run';
 import refundAccount from './continuous/refundAccount/run';
 import refundCreditCard from './continuous/refundCreditCard/run';
 import refundMovieTicket from './continuous/refundMovieTicket/run';
 import registerProgramMembership from './continuous/registerProgramMembership/run';
-import retryTasks from './continuous/retryTasks/run';
 import returnOrder from './continuous/returnOrder/run';
 import returnPointAward from './continuous/returnPointAward/run';
 import sendEmailMessage from './continuous/sendEmailMessage/run';
 import sendOrder from './continuous/sendOrder/run';
 import triggerWebhook from './continuous/triggerWebhook/run';
 import unRegisterProgramMembership from './continuous/unRegisterProgramMembership/run';
+
 import createImportScreeningEventsTask from './triggered/createImportScreeningEventsTask/run';
+import updateScreeningEventAvailability from './triggered/updateScreeningEventAvailability/run';
 
 export default async () => {
     await abortTasks();
+    await makeTransactionExpired();
+    await reexportTransactionTasks();
+    await retryTasks();
+
+    await onCanceledPlaceOrder();
+    await onCanceledReturnOrder();
+    await onConfirmedPlaceOrder();
+    await onConfirmedReturnOrder();
+    await onExpiredPlaceOrder();
+    await onExpiredReturnOrder();
+
     await cancelAccount();
     await cancelCreditCard();
     await cancelPointAward();
@@ -43,23 +58,14 @@ export default async () => {
     await confirmReservation();
     await givePointAward();
     await importScreeningEvents();
-    await makeTransactionExpired();
-    await onCanceledPlaceOrder();
-    await onCanceledReturnOrder();
-    await onConfirmedPlaceOrder();
-    await onConfirmedReturnOrder();
-    await onExpiredPlaceOrder();
-    await onExpiredReturnOrder();
     await payAccount();
     await payCreditCard();
     await payMovieTicket();
     await placeOrder();
-    await reexportTransactionTasks();
     await refundAccount();
     await refundCreditCard();
     await refundMovieTicket();
     await registerProgramMembership();
-    await retryTasks();
     await returnOrder();
     await returnPointAward();
     await sendEmailMessage();
@@ -68,4 +74,5 @@ export default async () => {
     await unRegisterProgramMembership();
 
     await createImportScreeningEventsTask();
+    await updateScreeningEventAvailability();
 };
