eb41a699bb99eae096baa9cb2fa186f45576764b
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2019-11-05 23:06:24 +0000

    ### Changed - ttts注文照会をCinerino化

--- dst/app/routes/ttts/orders.js
+++ dst/app/routes/ttts/orders.js
@@ -21,7 +21,7 @@ const authentication_1 = require("../../middlewares/authentication");
 const permitScopes_1 = require("../../middlewares/permitScopes");
 const validator_1 = require("../../middlewares/validator");
 const redis = require("../../../redis");
-const reservation_1 = require("../../util/reservation");
+// import { tttsReservation2chevre } from '../../util/reservation';
 /**
  * 正規表現をエスケープする
  */
@@ -33,55 +33,66 @@ ordersRouter.use(authentication_1.default);
 /**
  * make inquiry of an order
  */
-ordersRouter.post('/findByOrderInquiryKey', permitScopes_1.default(['orders', 'orders.read-only']), ...[
-    check_1.body('performanceDay')
-        .not()
-        .isEmpty()
-        .withMessage(() => 'required'),
-    check_1.body('paymentNo')
-        .not()
-        .isEmpty()
-        .withMessage(() => 'required'),
-    check_1.body('telephone')
-        .not()
-        .isEmpty()
-        .withMessage(() => 'required')
-], validator_1.default, (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const key = {
-            performanceDay: req.body.performanceDay,
-            paymentNo: req.body.paymentNo,
+ordersRouter.post('/findByOrderInquiryKey', permitScopes_1.default(['orders', 'orders.read-only']), 
+// 互換性維持のため
+(req, _, next) => {
+    if (typeof req.body.performanceDay === 'string' && typeof req.body.paymentNo === 'string') {
+        req.body.confirmationNumber = `${req.body.performanceDay}${req.body.paymentNo}`;
+    }
+    if (typeof req.body.telephone === 'string') {
+        req.body.customer = {
             telephone: req.body.telephone
         };
+    }
+    next();
+}, ...[
+    check_1.body('confirmationNumber')
+        .not()
+        .isEmpty()
+        .withMessage((_, __) => 'required'),
+    check_1.body('customer')
+        .not()
+        .isEmpty()
+        .withMessage((_, __) => 'required')
+], validator_1.default, (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        // const key = {
+        //     performanceDay: <string>req.body.performanceDay,
+        //     paymentNo: <string>req.body.paymentNo,
+        //     telephone: <string>req.body.telephone
+        // };
         const orderRepo = new cinerino.repository.Order(mongoose.connection);
         let order;
         const orders = yield orderRepo.search({
             limit: 1,
             sort: { orderDate: cinerino.factory.sortType.Descending },
-            customer: { telephone: `${escapeRegExp(key.telephone)}$` },
-            confirmationNumbers: [`${key.performanceDay}${key.paymentNo}`]
+            customer: { telephone: `${escapeRegExp(req.body.customer.telephone)}$` },
+            confirmationNumbers: [req.body.confirmationNumber]
         });
         order = orders.shift();
         if (order === undefined) {
             // まだ注文が作成されていなければ、注文取引から検索するか検討中だが、いまのところ取引検索条件が足りない...
             throw new cinerino.factory.errors.NotFound('Order');
         }
-        order.acceptedOffers = order.acceptedOffers
-            // 余分確保分を除く
-            .filter((o) => {
-            const reservation = o.itemOffered;
-            let extraProperty;
-            if (reservation.additionalProperty !== undefined) {
-                extraProperty = reservation.additionalProperty.find((p) => p.name === 'extra');
-            }
-            return reservation.additionalProperty === undefined
-                || extraProperty === undefined
-                || extraProperty.value !== '1';
-        })
-            // 互換性維持
-            .map((o) => {
-            return Object.assign(Object.assign({}, o), { itemOffered: reservation_1.tttsReservation2chevre(o.itemOffered) });
-        });
+        // order.acceptedOffers = order.acceptedOffers
+        //     // 余分確保分を除く
+        //     .filter((o) => {
+        //         const reservation = <cinerino.factory.order.IReservation>o.itemOffered;
+        //         let extraProperty: cinerino.factory.propertyValue.IPropertyValue<string> | undefined;
+        //         if (reservation.additionalProperty !== undefined) {
+        //             extraProperty = reservation.additionalProperty.find((p) => p.name === 'extra');
+        //         }
+        //         return reservation.additionalProperty === undefined
+        //             || extraProperty === undefined
+        //             || extraProperty.value !== '1';
+        //     })
+        //     // 互換性維持
+        //     .map((o) => {
+        //         return {
+        //             ...o,
+        //             itemOffered: tttsReservation2chevre(<cinerino.factory.order.IReservation>o.itemOffered)
+        //         };
+        //     });
         // 印刷トークンを発行
         const tokenRepo = new cinerino.repository.Token(redis.getClient());
         const reservationIds = order.acceptedOffers.map((o) => o.itemOffered.id);
--- src/app/routes/ttts/orders.ts
+++ src/app/routes/ttts/orders.ts
@@ -13,7 +13,7 @@ import validator from '../../middlewares/validator';
 
 import * as redis from '../../../redis';
 
-import { tttsReservation2chevre } from '../../util/reservation';
+// import { tttsReservation2chevre } from '../../util/reservation';
 
 /**
  * 正規表現をエスケープする
@@ -31,29 +31,38 @@ ordersRouter.use(authentication);
 ordersRouter.post(
     '/findByOrderInquiryKey',
     permitScopes(['orders', 'orders.read-only']),
-    ...[
-        body('performanceDay')
-            .not()
-            .isEmpty()
-            .withMessage(() => 'required'),
-        body('paymentNo')
-            .not()
-            .isEmpty()
-            .withMessage(() => 'required'),
-        body('telephone')
-            .not()
-            .isEmpty()
-            .withMessage(() => 'required')
+    // 互換性維持のため
+    (req, _, next) => {
+        if (typeof req.body.performanceDay === 'string' && typeof req.body.paymentNo === 'string') {
+            req.body.confirmationNumber = `${req.body.performanceDay}${req.body.paymentNo}`;
+        }
 
+        if (typeof req.body.telephone === 'string') {
+            req.body.customer = {
+                telephone: req.body.telephone
+            };
+        }
+
+        next();
+    },
+    ...[
+        body('confirmationNumber')
+            .not()
+            .isEmpty()
+            .withMessage((_, __) => 'required'),
+        body('customer')
+            .not()
+            .isEmpty()
+            .withMessage((_, __) => 'required')
     ],
     validator,
     async (req, res, next) => {
         try {
-            const key = {
-                performanceDay: <string>req.body.performanceDay,
-                paymentNo: <string>req.body.paymentNo,
-                telephone: <string>req.body.telephone
-            };
+            // const key = {
+            //     performanceDay: <string>req.body.performanceDay,
+            //     paymentNo: <string>req.body.paymentNo,
+            //     telephone: <string>req.body.telephone
+            // };
 
             const orderRepo = new cinerino.repository.Order(mongoose.connection);
             let order: cinerino.factory.order.IOrder | undefined;
@@ -61,8 +70,8 @@ ordersRouter.post(
             const orders = await orderRepo.search({
                 limit: 1,
                 sort: { orderDate: cinerino.factory.sortType.Descending },
-                customer: { telephone: `${escapeRegExp(key.telephone)}$` },
-                confirmationNumbers: [`${key.performanceDay}${key.paymentNo}`]
+                customer: { telephone: `${escapeRegExp(req.body.customer.telephone)}$` },
+                confirmationNumbers: [<string>req.body.confirmationNumber]
             });
             order = orders.shift();
             if (order === undefined) {
@@ -70,26 +79,26 @@ ordersRouter.post(
                 throw new cinerino.factory.errors.NotFound('Order');
             }
 
-            order.acceptedOffers = order.acceptedOffers
-                // 余分確保分を除く
-                .filter((o) => {
-                    const reservation = <cinerino.factory.order.IReservation>o.itemOffered;
-                    let extraProperty: cinerino.factory.propertyValue.IPropertyValue<string> | undefined;
-                    if (reservation.additionalProperty !== undefined) {
-                        extraProperty = reservation.additionalProperty.find((p) => p.name === 'extra');
-                    }
+            // order.acceptedOffers = order.acceptedOffers
+            //     // 余分確保分を除く
+            //     .filter((o) => {
+            //         const reservation = <cinerino.factory.order.IReservation>o.itemOffered;
+            //         let extraProperty: cinerino.factory.propertyValue.IPropertyValue<string> | undefined;
+            //         if (reservation.additionalProperty !== undefined) {
+            //             extraProperty = reservation.additionalProperty.find((p) => p.name === 'extra');
+            //         }
 
-                    return reservation.additionalProperty === undefined
-                        || extraProperty === undefined
-                        || extraProperty.value !== '1';
-                })
-                // 互換性維持
-                .map((o) => {
-                    return {
-                        ...o,
-                        itemOffered: tttsReservation2chevre(<cinerino.factory.order.IReservation>o.itemOffered)
-                    };
-                });
+            //         return reservation.additionalProperty === undefined
+            //             || extraProperty === undefined
+            //             || extraProperty.value !== '1';
+            //     })
+            //     // 互換性維持
+            //     .map((o) => {
+            //         return {
+            //             ...o,
+            //             itemOffered: tttsReservation2chevre(<cinerino.factory.order.IReservation>o.itemOffered)
+            //         };
+            //     });
 
             // 印刷トークンを発行
             const tokenRepo = new cinerino.repository.Token(redis.getClient());
