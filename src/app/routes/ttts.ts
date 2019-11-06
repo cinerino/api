8770862be3952f0ea770d927b1b4b989ef6c1277
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2019-11-06 03:59:48 +0000

    ### Removed - ttts注文返品取引ルーターを削除 - ttts注文ルーターを削除

--- src/app/routes/ttts.ts
+++ src/app/routes/ttts.ts
@@ -5,14 +5,10 @@ import * as cinerino from '@cinerino/domain';
 
 import { Router } from 'express';
 
-import ordersRouter from './ttts/orders';
 import placeOrderTransactionsRouter from './ttts/transactions/placeOrder';
-import returnOrderTransactionsRouter from './ttts/transactions/returnOrder';
 
 const tttsRouter = Router();
 
-tttsRouter.use('/orders', ordersRouter);
 tttsRouter.use(`/transactions/${cinerino.factory.transactionType.PlaceOrder}`, placeOrderTransactionsRouter);
-tttsRouter.use(`/transactions/${cinerino.factory.transactionType.ReturnOrder}`, returnOrderTransactionsRouter);
 
 export default tttsRouter;
