121f98945b93a835a2bf2923f18d35f2999355aa
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2019-12-28 08:26:31 +0000

    ### Added - 組織ルーターを追加

--- dst/app/routes/organizations.js
+++ dst/app/routes/organizations.js
@@ -18,65 +18,25 @@ const mongoose = require("mongoose");
 const permitScopes_1 = require("../middlewares/permitScopes");
 const rateLimit_1 = require("../middlewares/rateLimit");
 const validator_1 = require("../middlewares/validator");
-// type ICreditCardPaymentAccepted = cinerino.factory.seller.IPaymentAccepted<cinerino.factory.paymentMethodType.CreditCard>;
 const organizationsRouter = express_1.Router();
 /**
  * @deprecated Use /sellers
  */
 organizationsRouter.get('/movieTheater', permitScopes_1.default(['customer', 'organizations', 'organizations.read-only']), rateLimit_1.default, validator_1.default, (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const searchCoinditions = Object.assign(Object.assign({}, req.query), { 
+        const searchCoinditions = Object.assign(Object.assign({}, req.query), { project: { ids: [req.project.id] }, 
             // tslint:disable-next-line:no-magic-numbers
             limit: (req.query.limit !== undefined) ? Math.min(req.query.limit, 100) : 100, page: (req.query.page !== undefined) ? Math.max(req.query.page, 1) : 1 });
         const sellerRepo = new cinerino.repository.Seller(mongoose.connection);
-        const movieTheaters = yield sellerRepo.search(searchCoinditions, 
+        const sellers = yield sellerRepo.search(searchCoinditions, 
         // 管理者以外にセキュアな情報を露出しないように
         (!req.isAdmin) ? { 'paymentAccepted.gmoInfo.shopPass': 0 } : undefined);
         const totalCount = yield sellerRepo.count(searchCoinditions);
-        // movieTheaters.forEach((movieTheater) => {
-        //     // 互換性維持のためgmoInfoをpaymentAcceptedから情報追加
-        //     if (Array.isArray(movieTheater.paymentAccepted)) {
-        //         const creditCardPaymentAccepted = <ICreditCardPaymentAccepted>movieTheater.paymentAccepted.find((p) => {
-        //             return p.paymentMethodType === cinerino.factory.paymentMethodType.CreditCard;
-        //         });
-        //         if (creditCardPaymentAccepted !== undefined) {
-        //             (<any>movieTheater).gmoInfo = creditCardPaymentAccepted.gmoInfo;
-        //         }
-        //     }
-        // });
         res.set('X-Total-Count', totalCount.toString());
-        res.json(movieTheaters);
+        res.json(sellers);
     }
     catch (error) {
         next(error);
     }
 }));
-/**
- * @deprecated Use /sellers
- */
-// organizationsRouter.get(
-//     '/movieTheater/:branchCode([0-9]{3})',
-//     permitScopes(['customer', 'organizations', 'organizations.read-only']),
-//     rateLimit,
-//     validator,
-//     async (req, res, next) => {
-//         try {
-//             const sellerRepo = new cinerino.repository.Seller(mongoose.connection);
-//             const movieTheaters = await sellerRepo.search(
-//                 {
-//                     location: { branchCodes: [req.params.branchCode] }
-//                 },
-//                 // 管理者以外にセキュアな情報を露出しないように
-//                 (!req.isAdmin) ? { 'paymentAccepted.gmoInfo.shopPass': 0 } : undefined
-//             );
-//             const movieTheater = movieTheaters.shift();
-//             if (movieTheater === undefined) {
-//                 throw new cinerino.factory.errors.NotFound('Organization');
-//             }
-//             res.json(movieTheater);
-//         } catch (error) {
-//             next(error);
-//         }
-//     }
-// );
 exports.default = organizationsRouter;
--- src/app/routes/organizations.ts
+++ src/app/routes/organizations.ts
@@ -9,8 +9,6 @@ import permitScopes from '../middlewares/permitScopes';
 import rateLimit from '../middlewares/rateLimit';
 import validator from '../middlewares/validator';
 
-// type ICreditCardPaymentAccepted = cinerino.factory.seller.IPaymentAccepted<cinerino.factory.paymentMethodType.CreditCard>;
-
 const organizationsRouter = Router();
 
 /**
@@ -25,67 +23,26 @@ organizationsRouter.get(
         try {
             const searchCoinditions: cinerino.factory.seller.ISearchConditions = {
                 ...req.query,
+                project: { ids: [req.project.id] },
                 // tslint:disable-next-line:no-magic-numbers
                 limit: (req.query.limit !== undefined) ? Math.min(req.query.limit, 100) : 100,
                 page: (req.query.page !== undefined) ? Math.max(req.query.page, 1) : 1
             };
 
             const sellerRepo = new cinerino.repository.Seller(mongoose.connection);
-            const movieTheaters = await sellerRepo.search(
+            const sellers = await sellerRepo.search(
                 searchCoinditions,
                 // 管理者以外にセキュアな情報を露出しないように
                 (!req.isAdmin) ? { 'paymentAccepted.gmoInfo.shopPass': 0 } : undefined
             );
             const totalCount = await sellerRepo.count(searchCoinditions);
 
-            // movieTheaters.forEach((movieTheater) => {
-            //     // 互換性維持のためgmoInfoをpaymentAcceptedから情報追加
-            //     if (Array.isArray(movieTheater.paymentAccepted)) {
-            //         const creditCardPaymentAccepted = <ICreditCardPaymentAccepted>movieTheater.paymentAccepted.find((p) => {
-            //             return p.paymentMethodType === cinerino.factory.paymentMethodType.CreditCard;
-            //         });
-            //         if (creditCardPaymentAccepted !== undefined) {
-            //             (<any>movieTheater).gmoInfo = creditCardPaymentAccepted.gmoInfo;
-            //         }
-            //     }
-            // });
-
             res.set('X-Total-Count', totalCount.toString());
-            res.json(movieTheaters);
+            res.json(sellers);
         } catch (error) {
             next(error);
         }
     }
 );
-
-/**
- * @deprecated Use /sellers
- */
-// organizationsRouter.get(
-//     '/movieTheater/:branchCode([0-9]{3})',
-//     permitScopes(['customer', 'organizations', 'organizations.read-only']),
-//     rateLimit,
-//     validator,
-//     async (req, res, next) => {
-//         try {
-//             const sellerRepo = new cinerino.repository.Seller(mongoose.connection);
-//             const movieTheaters = await sellerRepo.search(
-//                 {
-//                     location: { branchCodes: [req.params.branchCode] }
-//                 },
-//                 // 管理者以外にセキュアな情報を露出しないように
-//                 (!req.isAdmin) ? { 'paymentAccepted.gmoInfo.shopPass': 0 } : undefined
-//             );
-//             const movieTheater = movieTheaters.shift();
-//             if (movieTheater === undefined) {
-//                 throw new cinerino.factory.errors.NotFound('Organization');
-//             }
-
-//             res.json(movieTheater);
-//         } catch (error) {
-//             next(error);
-//         }
-//     }
-// );
 
 export default organizationsRouter;
