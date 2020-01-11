e96deca3814e3f3f1f23089ca985985ad01f6cbf
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-01-11 09:46:46 +0000

    ### Changed - IAMロールリポジトリを追加

---
+++ dst/app/routes/iam/roles.js
@@ -0,0 +1,40 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * IAMロールルーター
+ */
+const cinerino = require("@cinerino/domain");
+const express = require("express");
+const mongoose = require("mongoose");
+const permitScopes_1 = require("../../middlewares/permitScopes");
+const rateLimit_1 = require("../../middlewares/rateLimit");
+const validator_1 = require("../../middlewares/validator");
+const iamRolesRouter = express.Router();
+/**
+ * IAMロール検索
+ */
+iamRolesRouter.get('', permitScopes_1.default([]), rateLimit_1.default, validator_1.default, (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const searchCoinditions = Object.assign(Object.assign({}, req.query), { 
+            // tslint:disable-next-line:no-magic-numbers
+            limit: (req.query.limit !== undefined) ? Math.min(req.query.limit, 100) : 100, page: (req.query.page !== undefined) ? Math.max(req.query.page, 1) : 1 });
+        const roleRepo = new cinerino.repository.Role(mongoose.connection);
+        const roles = yield roleRepo.search(searchCoinditions);
+        const totalCount = yield roleRepo.count(searchCoinditions);
+        res.set('X-Total-Count', totalCount.toString());
+        res.json(roles);
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+exports.default = iamRolesRouter;
