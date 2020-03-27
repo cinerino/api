a0da77f19207018b1ee781f2a880ae48999666e1
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-03-27 23:46:18 +0000

    ### Changed - MongoDBコネクション監視調整

--- dst/connectMongo.js
+++ dst/connectMongo.js
@@ -16,9 +16,14 @@ const cinerino = require("@cinerino/domain");
 const createDebug = require("debug");
 const mongoose = require("mongoose");
 const debug = createDebug('cinerino-api:connectMongo');
-const PING_INTERVAL = 10000;
 const MONGOLAB_URI = process.env.MONGOLAB_URI;
 const AUTO_INDEX = process.env.MONGO_AUTO_INDEX_DISABLED !== '1';
+const MONGO_PING_INTERVAL_MS = (typeof process.env.MONGO_PING_INTERVAL_MS === 'string')
+    ? Number(process.env.MONGO_PING_INTERVAL_MS)
+    // tslint:disable-next-line:no-magic-numbers
+    : 30000;
+// tslint:disable-next-line:no-magic-numbers
+const MONGO_PING_TIMEOUT_MS = (typeof process.env.MONGO_PING_TIMEOUT_MS === 'string') ? Number(process.env.MONGO_PING_TIMEOUT_MS) : 10000;
 const connectOptions = {
     autoIndex: AUTO_INDEX,
     autoReconnect: true,
@@ -61,15 +66,15 @@ function connectMongo(params) {
                             })
                                 .catch((error) => {
                                 // tslint:disable-next-line:no-console
-                                console.error('ping:', error);
+                                console.error('ping error:', error);
                             });
                         }
                         catch (error) {
                             // tslint:disable-next-line:no-console
-                            console.error(error);
+                            console.error('connection.db.admin() error:', error);
                         }
                         // tslint:disable-next-line:no-magic-numbers
-                        setTimeout(() => { resolve(); }, 5000);
+                        setTimeout(() => { resolve(); }, MONGO_PING_TIMEOUT_MS);
                     });
                     // 疎通確認結果が適性であれば何もしない
                     if (pingResult !== undefined && pingResult.ok === 1) {
@@ -88,7 +93,7 @@ function connectMongo(params) {
                     console.error('mongoose.connect:', error);
                     yield cinerino.service.notification.report2developers(`[${process.env.PROJECT_ID}] api:connectMongo`, `MongoDB connection error: ${error.stack}`)();
                 }
-            }), PING_INTERVAL);
+            }), MONGO_PING_INTERVAL_MS);
         }
         return connection;
     });
--- src/connectMongo.ts
+++ src/connectMongo.ts
@@ -6,9 +6,14 @@ import * as createDebug from 'debug';
 import * as mongoose from 'mongoose';
 
 const debug = createDebug('cinerino-api:connectMongo');
-const PING_INTERVAL = 10000;
 const MONGOLAB_URI = <string>process.env.MONGOLAB_URI;
 const AUTO_INDEX = process.env.MONGO_AUTO_INDEX_DISABLED !== '1';
+const MONGO_PING_INTERVAL_MS = (typeof process.env.MONGO_PING_INTERVAL_MS === 'string')
+    ? Number(process.env.MONGO_PING_INTERVAL_MS)
+    // tslint:disable-next-line:no-magic-numbers
+    : 30000;
+// tslint:disable-next-line:no-magic-numbers
+const MONGO_PING_TIMEOUT_MS = (typeof process.env.MONGO_PING_TIMEOUT_MS === 'string') ? Number(process.env.MONGO_PING_TIMEOUT_MS) : 10000;
 
 const connectOptions: mongoose.ConnectionOptions = {
     autoIndex: AUTO_INDEX,
@@ -56,15 +61,15 @@ export async function connectMongo(params: {
                                 })
                                 .catch((error) => {
                                     // tslint:disable-next-line:no-console
-                                    console.error('ping:', error);
+                                    console.error('ping error:', error);
                                 });
                         } catch (error) {
                             // tslint:disable-next-line:no-console
-                            console.error(error);
+                            console.error('connection.db.admin() error:', error);
                         }
 
                         // tslint:disable-next-line:no-magic-numbers
-                        setTimeout(() => { resolve(); }, 5000);
+                        setTimeout(() => { resolve(); }, MONGO_PING_TIMEOUT_MS);
                     });
 
                     // 疎通確認結果が適性であれば何もしない
@@ -91,7 +96,7 @@ export async function connectMongo(params: {
                     )();
                 }
             },
-            PING_INTERVAL
+            MONGO_PING_INTERVAL_MS
         );
     }
 
