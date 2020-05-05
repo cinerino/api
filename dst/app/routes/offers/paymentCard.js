df9d17f71eb0acf6070a4b511c463f3455438511
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-05-05 06:02:54 +0000

    ### Added - 注文アイテムに決済カードを追加

---
+++ dst/app/routes/offers/paymentCard.js
@@ -0,0 +1,157 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * オファールーター
+ */
+const cinerino = require("@cinerino/domain");
+const express_1 = require("express");
+const express_validator_1 = require("express-validator");
+const http_status_1 = require("http-status");
+const mongoose = require("mongoose");
+const redis = require("../../../redis");
+const lockTransaction_1 = require("../../middlewares/lockTransaction");
+const permitScopes_1 = require("../../middlewares/permitScopes");
+const rateLimit_1 = require("../../middlewares/rateLimit");
+const rateLimit4transactionInProgress_1 = require("../../middlewares/rateLimit4transactionInProgress");
+const validator_1 = require("../../middlewares/validator");
+const ADDITIONAL_PROPERTY_VALUE_MAX_LENGTH = (process.env.ADDITIONAL_PROPERTY_VALUE_MAX_LENGTH !== undefined)
+    ? Number(process.env.ADDITIONAL_PROPERTY_VALUE_MAX_LENGTH)
+    // tslint:disable-next-line:no-magic-numbers
+    : 256;
+const paymentCardOffersRouter = express_1.Router();
+// tslint:disable-next-line:use-default-type-parameter
+paymentCardOffersRouter.post('/authorize', permitScopes_1.default(['transactions']), rateLimit_1.default, ...[
+    express_validator_1.body('object.id')
+        .not()
+        .isEmpty()
+        .withMessage(() => 'required'),
+    express_validator_1.body('object.itemOffered.id')
+        .not()
+        .isEmpty()
+        .withMessage(() => 'required'),
+    express_validator_1.body('object.itemOffered.serviceOutput.accessCode')
+        .not()
+        .isEmpty()
+        .withMessage(() => 'required'),
+    express_validator_1.body('object.itemOffered.serviceOutput.additionalProperty')
+        .optional()
+        .isArray({ max: 10 }),
+    express_validator_1.body('object.itemOffered.serviceOutput.additionalProperty.*.name')
+        .optional()
+        .not()
+        .isEmpty()
+        .isString()
+        .isLength({ max: ADDITIONAL_PROPERTY_VALUE_MAX_LENGTH }),
+    express_validator_1.body('object.itemOffered.serviceOutput.additionalProperty.*.value')
+        .optional()
+        .not()
+        .isEmpty()
+        .isString()
+        .isLength({ max: ADDITIONAL_PROPERTY_VALUE_MAX_LENGTH }),
+    express_validator_1.body('purpose')
+        .not()
+        .isEmpty()
+], validator_1.default, (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    yield rateLimit4transactionInProgress_1.default({
+        typeOf: req.body.purpose.typeOf,
+        id: req.body.purpose.id
+    })(req, res, next);
+}), (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    yield lockTransaction_1.default({
+        typeOf: req.body.purpose.typeOf,
+        id: req.body.purpose.id
+    })(req, res, next);
+}), (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a, _b, _c, _d, _e, _f, _g, _h, _j;
+    try {
+        const action = yield cinerino.service.offer.paymentCard.authorize({
+            project: req.project,
+            object: {
+                typeOf: cinerino.factory.chevre.offerType.Offer,
+                id: (_a = req.body.object) === null || _a === void 0 ? void 0 : _a.id,
+                itemOffered: {
+                    id: (_c = (_b = req.body.object) === null || _b === void 0 ? void 0 : _b.itemOffered) === null || _c === void 0 ? void 0 : _c.id,
+                    serviceOutput: {
+                        // identifier: identifier,
+                        accessCode: (_f = (_e = (_d = req.body.object) === null || _d === void 0 ? void 0 : _d.itemOffered) === null || _e === void 0 ? void 0 : _e.serviceOutput) === null || _f === void 0 ? void 0 : _f.accessCode,
+                        name: (_j = (_h = (_g = req.body.object) === null || _g === void 0 ? void 0 : _g.itemOffered) === null || _h === void 0 ? void 0 : _h.serviceOutput) === null || _j === void 0 ? void 0 : _j.name
+                        // additionalProperty: [
+                        //     { name: 'accountType', value: 'Prepaid' },
+                        //     { name: 'accountNumber', value: identifier },
+                        // ]
+                    }
+                }
+                // additionalProperty: (Array.isArray(req.body.object.additionalProperty))
+                //     ? (<any[]>req.body.object.additionalProperty).map((p: any) => {
+                //         return { name: String(p.name), value: String(p.value) };
+                //     })
+                //     : [],
+            },
+            agent: { id: req.user.sub },
+            transaction: { typeOf: req.body.purpose.typeOf, id: req.body.purpose.id }
+        })({
+            accountNumber: new cinerino.repository.AccountNumber(redis.getClient()),
+            action: new cinerino.repository.Action(mongoose.connection),
+            project: new cinerino.repository.Project(mongoose.connection),
+            seller: new cinerino.repository.Seller(mongoose.connection),
+            transaction: new cinerino.repository.Transaction(mongoose.connection)
+        });
+        res.status(http_status_1.CREATED)
+            .json(action);
+    }
+    catch (error) {
+        next(error);
+    }
+}));
+// tslint:disable-next-line:use-default-type-parameter
+// paymentCardOffersRouter.put<ParamsDictionary>(
+//     '/authorize/:actionId/void',
+//     permitScopes(['transactions']),
+//     rateLimit,
+//     ...[
+//         body('purpose')
+//             .not()
+//             .isEmpty()
+//     ],
+//     validator,
+//     async (req, res, next) => {
+//         await rateLimit4transactionInProgress({
+//             typeOf: req.body.purpose.typeOf,
+//             id: <string>req.body.purpose.id
+//         })(req, res, next);
+//     },
+//     async (req, res, next) => {
+//         await lockTransaction({
+//             typeOf: req.body.purpose.typeOf,
+//             id: <string>req.body.purpose.id
+//         })(req, res, next);
+//     },
+//     async (req, res, next) => {
+//         try {
+//             await cinerino.service.offer.paymentCard.voidTransaction({
+//                 project: req.project,
+//                 id: req.params.actionId,
+//                 agent: { id: req.user.sub },
+//                 purpose: { typeOf: req.body.purpose.typeOf, id: <string>req.body.purpose.id }
+//             })({
+//                 action: new cinerino.repository.Action(mongoose.connection),
+//                 project: new cinerino.repository.Project(mongoose.connection),
+//                 transaction: new cinerino.repository.Transaction(mongoose.connection)
+//             });
+//             res.status(NO_CONTENT)
+//                 .end();
+//         } catch (error) {
+//             next(error);
+//         }
+//     }
+// );
+exports.default = paymentCardOffersRouter;
---
+++ src/app/routes/offers/paymentCard.ts
@@ -0,0 +1,164 @@
+/**
+ * オファールーター
+ */
+import * as cinerino from '@cinerino/domain';
+import { Router } from 'express';
+// tslint:disable-next-line:no-implicit-dependencies
+import { ParamsDictionary } from 'express-serve-static-core';
+import { body } from 'express-validator';
+import { CREATED } from 'http-status';
+import * as mongoose from 'mongoose';
+
+import * as redis from '../../../redis';
+
+import lockTransaction from '../../middlewares/lockTransaction';
+import permitScopes from '../../middlewares/permitScopes';
+import rateLimit from '../../middlewares/rateLimit';
+import rateLimit4transactionInProgress from '../../middlewares/rateLimit4transactionInProgress';
+import validator from '../../middlewares/validator';
+
+const ADDITIONAL_PROPERTY_VALUE_MAX_LENGTH = (process.env.ADDITIONAL_PROPERTY_VALUE_MAX_LENGTH !== undefined)
+    ? Number(process.env.ADDITIONAL_PROPERTY_VALUE_MAX_LENGTH)
+    // tslint:disable-next-line:no-magic-numbers
+    : 256;
+
+const paymentCardOffersRouter = Router();
+
+// tslint:disable-next-line:use-default-type-parameter
+paymentCardOffersRouter.post<ParamsDictionary>(
+    '/authorize',
+    permitScopes(['transactions']),
+    rateLimit,
+    ...[
+        body('object.id')
+            .not()
+            .isEmpty()
+            .withMessage(() => 'required'),
+        body('object.itemOffered.id')
+            .not()
+            .isEmpty()
+            .withMessage(() => 'required'),
+        body('object.itemOffered.serviceOutput.accessCode')
+            .not()
+            .isEmpty()
+            .withMessage(() => 'required'),
+        body('object.itemOffered.serviceOutput.additionalProperty')
+            .optional()
+            .isArray({ max: 10 }),
+        body('object.itemOffered.serviceOutput.additionalProperty.*.name')
+            .optional()
+            .not()
+            .isEmpty()
+            .isString()
+            .isLength({ max: ADDITIONAL_PROPERTY_VALUE_MAX_LENGTH }),
+        body('object.itemOffered.serviceOutput.additionalProperty.*.value')
+            .optional()
+            .not()
+            .isEmpty()
+            .isString()
+            .isLength({ max: ADDITIONAL_PROPERTY_VALUE_MAX_LENGTH }),
+        body('purpose')
+            .not()
+            .isEmpty()
+    ],
+    validator,
+    async (req, res, next) => {
+        await rateLimit4transactionInProgress({
+            typeOf: req.body.purpose.typeOf,
+            id: <string>req.body.purpose.id
+        })(req, res, next);
+    },
+    async (req, res, next) => {
+        await lockTransaction({
+            typeOf: req.body.purpose.typeOf,
+            id: <string>req.body.purpose.id
+        })(req, res, next);
+    },
+    async (req, res, next) => {
+        try {
+            const action = await cinerino.service.offer.paymentCard.authorize({
+                project: req.project,
+                object: {
+                    typeOf: cinerino.factory.chevre.offerType.Offer,
+                    id: req.body.object?.id,
+                    itemOffered: {
+                        id: req.body.object?.itemOffered?.id,
+                        serviceOutput: {
+                            // identifier: identifier,
+                            accessCode: req.body.object?.itemOffered?.serviceOutput?.accessCode,
+                            name: req.body.object?.itemOffered?.serviceOutput?.name
+                            // additionalProperty: [
+                            //     { name: 'accountType', value: 'Prepaid' },
+                            //     { name: 'accountNumber', value: identifier },
+                            // ]
+                        }
+                    }
+                    // additionalProperty: (Array.isArray(req.body.object.additionalProperty))
+                    //     ? (<any[]>req.body.object.additionalProperty).map((p: any) => {
+                    //         return { name: String(p.name), value: String(p.value) };
+                    //     })
+                    //     : [],
+                },
+                agent: { id: req.user.sub },
+                transaction: <any>{ typeOf: req.body.purpose.typeOf, id: <string>req.body.purpose.id }
+            })({
+                accountNumber: new cinerino.repository.AccountNumber(redis.getClient()),
+                action: new cinerino.repository.Action(mongoose.connection),
+                project: new cinerino.repository.Project(mongoose.connection),
+                seller: new cinerino.repository.Seller(mongoose.connection),
+                transaction: new cinerino.repository.Transaction(mongoose.connection)
+            });
+
+            res.status(CREATED)
+                .json(action);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+// tslint:disable-next-line:use-default-type-parameter
+// paymentCardOffersRouter.put<ParamsDictionary>(
+//     '/authorize/:actionId/void',
+//     permitScopes(['transactions']),
+//     rateLimit,
+//     ...[
+//         body('purpose')
+//             .not()
+//             .isEmpty()
+//     ],
+//     validator,
+//     async (req, res, next) => {
+//         await rateLimit4transactionInProgress({
+//             typeOf: req.body.purpose.typeOf,
+//             id: <string>req.body.purpose.id
+//         })(req, res, next);
+//     },
+//     async (req, res, next) => {
+//         await lockTransaction({
+//             typeOf: req.body.purpose.typeOf,
+//             id: <string>req.body.purpose.id
+//         })(req, res, next);
+//     },
+//     async (req, res, next) => {
+//         try {
+//             await cinerino.service.offer.paymentCard.voidTransaction({
+//                 project: req.project,
+//                 id: req.params.actionId,
+//                 agent: { id: req.user.sub },
+//                 purpose: { typeOf: req.body.purpose.typeOf, id: <string>req.body.purpose.id }
+//             })({
+//                 action: new cinerino.repository.Action(mongoose.connection),
+//                 project: new cinerino.repository.Project(mongoose.connection),
+//                 transaction: new cinerino.repository.Transaction(mongoose.connection)
+//             });
+
+//             res.status(NO_CONTENT)
+//                 .end();
+//         } catch (error) {
+//             next(error);
+//         }
+//     }
+// );
+
+export default paymentCardOffersRouter;
