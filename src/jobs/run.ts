5b64d57886075fcbc30e72dc05f495321aae881d
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2020-08-24 22:57:05 +0000

    ### Removed - 個別決済タスクを削除 - 個別決済中止タスクを削除 - 個別返金タスクを削除

--- src/jobs/run.ts
+++ src/jobs/run.ts
@@ -10,9 +10,6 @@ import onTransactionCanceled from './continuous/onTransactionCanceled/run';
 import onTransactionConfirmed from './continuous/onTransactionConfirmed/run';
 import onTransactionExpired from './continuous/onTransactionExpired/run';
 
-import cancelAccount from './continuous/cancelAccount/run';
-import cancelCreditCard from './continuous/cancelCreditCard/run';
-import cancelPaymentCard from './continuous/cancelPaymentCard/run';
 import cancelReservation from './continuous/cancelReservation/run';
 import cancelSeatReservation from './continuous/cancelSeatReservation/run';
 import confirmReservation from './continuous/confirmReservation/run';
@@ -22,16 +19,8 @@ import givePointAward from './continuous/givePointAward/run';
 import moneyTransfer from './continuous/moneyTransfer/run';
 import orderProgramMembership from './continuous/orderProgramMembership/run';
 import pay from './continuous/pay/run';
-import payAccount from './continuous/payAccount/run';
-import payCreditCard from './continuous/payCreditCard/run';
-import payMovieTicket from './continuous/payMovieTicket/run';
-import payPaymentCard from './continuous/payPaymentCard/run';
 import placeOrder from './continuous/placeOrder/run';
 import refund from './continuous/refund/run';
-import refundAccount from './continuous/refundAccount/run';
-import refundCreditCard from './continuous/refundCreditCard/run';
-import refundMovieTicket from './continuous/refundMovieTicket/run';
-import refundPaymentCard from './continuous/refundPaymentCard/run';
 import registerProgramMembership from './continuous/registerProgramMembership/run';
 import registerService from './continuous/registerService/run';
 import returnOrder from './continuous/returnOrder/run';
@@ -43,8 +32,6 @@ import unRegisterProgramMembership from './continuous/unRegisterProgramMembershi
 import voidMoneyTransfer from './continuous/voidMoneyTransfer/run';
 import voidPayment from './continuous/voidPayment/run';
 import voidRegisterService from './continuous/voidRegisterService/run';
-
-const USE_LEGACY_PAYMENT_TASKS = process.env.USE_LEGACY_PAYMENT_TASKS === '1';
 
 export default async () => {
     await abortTasks({});
@@ -78,18 +65,4 @@ export default async () => {
     await voidMoneyTransfer({});
     await voidPayment({});
     await voidRegisterService({});
-
-    if (USE_LEGACY_PAYMENT_TASKS) {
-        await cancelAccount({});
-        await cancelCreditCard({});
-        await cancelPaymentCard({});
-        await payAccount({});
-        await payCreditCard({});
-        await payMovieTicket({});
-        await payPaymentCard({});
-        await refundAccount({});
-        await refundCreditCard({});
-        await refundMovieTicket({});
-        await refundPaymentCard({});
-    }
 };
