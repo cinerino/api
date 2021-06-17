909e553d8e362ec15b99a0cf5669e2b24a4531c8
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2021-06-17 07:11:45 +0000

    - 会員ログイン判定にreq.canReadPeopleMeを使用するように調整

--- dst/app/middlewares/requireMember.js
+++ dst/app/middlewares/requireMember.js
@@ -4,12 +4,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
  * 会員必須ミドルウェア
  */
 const cinerino = require("@cinerino/domain");
-const createDebug = require("debug");
-const debug = createDebug('cinerino-api:middlewares');
 exports.default = (req, __, next) => {
     // 会員としてログイン済みであればOK
     if (isMember(req)) {
-        debug('logged in as', req.user.sub);
         next();
     }
     else {
